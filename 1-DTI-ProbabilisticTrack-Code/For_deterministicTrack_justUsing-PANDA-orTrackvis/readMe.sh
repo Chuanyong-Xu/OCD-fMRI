8617e7516cb41af0417d95762bb0a59164c6bdc7
Chuanyong-Xu <63353359+Chuanyong-Xu@users.noreply.github.com>
2026-04-01 11:42:29 +0800

    Update readMe.sh to include DTK in instructions

--- 1-DTI-ProbabilisticTrack-Code/For_deterministicTrack_justUsing-PANDA-orTrackvis/readMe.sh
+++ 1-DTI-ProbabilisticTrack-Code/For_deterministicTrack_justUsing-PANDA-orTrackvis/readMe.sh
@@ -1,2 +1,2 @@
-#(1)Using "trackvis" software or "PANDA" toolbox based on MATLAB to do the old-fashion deterministic fiber reconstructing;
+#(1)Using "DTK and trackvis" software or "PANDA" toolbox based on MATLAB to do the old-fashion deterministic fiber reconstructing;
 #(2)Or try the popular MRtrix software.
